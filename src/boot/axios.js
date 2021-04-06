ff00edaee2108358ab049097fd20974be0c0497f
Daniel Kopecký <kopecky.d@gmail.com>
2021-04-06 08:15:06 +0200

    Homepage

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -2,3 +2,8 @@ import Vue from 'vue'
 import axios from 'axios'
 
 Vue.prototype.$axios = axios
+axios.defaults.headers.common.Accept = 'application/json'
+axios.defaults.headers.common['Content-Type'] = 'application/json'
+axios.defaults.xsrfCookieName = 'csrftoken'
+axios.defaults.xsrfHeaderName = 'X-CSRFToken'
+axios.defaults.withCredentials = true
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,4 +1,5 @@
 import { collection } from '@oarepo/invenio-api-vue-composition'
+import Home from 'pages/Home'
 
 const routes = [
   {
@@ -7,7 +8,8 @@ const routes = [
     children: [
       collection(
         {
-          path: '',
+          apiUrl: '/',
+          path: 'all',
           collectionCode: 'all',
           name: 'all',
           component: () => import('pages/ItemList'),
@@ -37,6 +39,55 @@ const routes = [
           }
         }
       )
+    ]
+  },
+  {
+    path: '/theses',
+    component: () => import('layouts/ItemListLayout'),
+    children: [
+      collection(
+        {
+          apiUrl: '/draft/',
+          path: '',
+          collectionCode: 'theses',
+          name: 'theses',
+          component: () => import('pages/ItemList'),
+          loadingComponent: 'viewer',
+          recordRouteName: (record) => {
+            // if (record.links.self.indexOf('draft') > 0) {
+            //   return 'draft-restorations/objects/record'
+            // } else {
+            //   return 'restorations/objects/record'
+            // }
+          },
+          meta: {
+            query: {
+            }
+          },
+          httpGetProps: {
+            dedupingInterval: 100,
+            revalidateDebounce: 0,
+            shouldRetryOnError: false,
+            keepData: (data, error, oldUrl, oldQuery, newUrl, newQuery, options) => {
+              if (oldUrl === newUrl) {
+                // collection not changed
+                return true
+              }
+              return false
+            }
+          }
+        }
+      )
+    ]
+  },
+  {
+    path: '/home',
+    component: () => import('layouts/HomepageLayout'),
+    children: [
+      {
+        path: '',
+        component: Home
+      }
     ]
   },
   // Always leave this as last one,
