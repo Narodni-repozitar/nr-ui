8ce46177d2b0ec05bfa5572fdecc73048a4780cc
Miroslav Bauer <mirekys@users.noreply.github.com>
2021-08-24 18:11:16 +0200

    implement dataset edit form

---
+++ src/components/ui/FileList.vue
@@ -0,0 +1,109 @@
+<template lang="pug">
+.row.q-gutter-lg
+  .col-grow(v-if="files.length")
+    q-table.sticky-name-column(
+      flat
+      :rows="files" row-key="file_id" :columns="fileColumns"
+      :no-data-label="$t('message.noFiles')")
+      template(v-slot:body-cell-download="props")
+        q-td(:props="props")
+          div
+            q-btn(@click="download(props.value)" outline color="primary" :label="$t('label.download')")
+  .col-auto.q-mt-md.q-pt-md(v-if="canUpload")
+    upload-data(:files="dataset.http.data.links.files")
+</template>
+
+<script>
+import UploadData from 'components/forms/steps/UploadData'
+import {computed, defineComponent} from 'vue'
+import {useI18n} from 'vue-i18n'
+import {format} from 'quasar'
+const { humanStorageSize } = format
+
+export default defineComponent({
+  name: 'FileList',
+  components: {UploadData},
+  props: {
+    dataset: {
+      type: Object,
+      required: true
+    }
+  },
+  setup(props) {
+    const {t} = useI18n()
+
+    const fileColumns = [
+      {
+        name: 'name',
+        required: true,
+        label: t('label.name'),
+        align: 'left',
+        field: row => row.key,
+        sortable: true
+      },
+      {
+        name: 'size',
+        required: true,
+        label: t('label.size'),
+        align: 'left',
+        field: row => row.size,
+        format: val => humanStorageSize(val),
+        sortable: true
+      },
+      {
+        name: 'type',
+        required: true,
+        label: t('label.type'),
+        align: 'left',
+        field: row => row.mime_type,
+        sortable: true
+      },
+      {
+        name: 'download',
+        align: 'left',
+        field: row => row.url,
+        required: true
+      }
+    ]
+
+    function download(url) {
+      window.open(`${url}?download`, '_blank')
+    }
+
+    const files = computed(() => {
+      return props.dataset.metadata._files?.filter(f => f.checksum && f.checksum !== '') || []
+    })
+
+    // TODO: add check of auth/state need provides
+    const canUpload = computed(() => {
+      return props.dataset.metadata['oarepo:draft']
+    })
+
+    const uploadUrl = computed(() => {
+      return props.dataset.http.data.links.files
+    })
+
+    return {download, files, canUpload, uploadUrl, fileColumns}
+  }
+})
+</script>
+
+<style lang="sass">
+.sticky-name-column
+  /* specifying max-width so the example can
+    highlight the sticky column on any browser window */
+  //max-width: 600px
+
+  thead tr:first-child th:first-child
+    /* bg color is important for th; just specify one */
+    background-color: #fff
+
+  td:first-child
+    background-color: var(--q-grey-2)
+
+  th:first-child,
+  td:first-child
+    position: sticky
+    left: 0
+    z-index: 1
+</style>
--- src/pages/forms/EditForm.vue
+++ src/pages/forms/EditForm.vue
@@ -2,42 +2,52 @@
 q-page.q-mt-lg.q-mx-lg-xl.full-height(padding)
   .column.justify-center.items-center
     .col.row.q-pb-md
-      .text-h3.gt-md
-        span {{ $t(header?.title) }}
-        q-icon.q-pl-md(color="primary" size="md" :name="header?.icon")
-      .text-h4.lt-lg.gt-sm.q-mt-none.q-mb-lg
-        span {{ $t(header?.title) }}
-        q-icon.q-pl-md(color="primary" size="sm" :name="header?.icon")
-      .text-h6.lt-md.q-mt-none.q-mb-md
-        span {{ $t(header?.title) }}
-        q-icon.q-pl-md(color="primary" size="xs" :name="header?.icon")
+      .text-h3.gt-md.self-baseline.text-center
+        p {{ $t(header?.title) }}
+        p
+          u.text-bold {{record.metadata? record.metadata.id : ''}}
+      .text-h4.lt-lg.gt-sm.q-mt-none.q-mb-lg.self-baseline.text-center
+        p {{ $t(header?.title) }}
+        p
+          u.text-bold {{record.metadata? record.metadata.id : ''}}
+      .text-h6.lt-md.q-mt-none.q-mb-md.self-baseline
+        p {{ $t(header?.title) }}
+        p
+          u.text-bold {{record.metadata? record.metadata.id : ''}}
     .q-separator(spaced)
-    create-dataset-form.col.q-pr-md
+    edit-dataset-form.col.q-pr-md(v-if="record.metadata" :record="record")
 </template>
 <script>
-import {computed, defineComponent} from 'vue'
+import {computed, defineComponent, ref} from 'vue'
 import {useMeta} from 'quasar'
 import {useI18n} from 'vue-i18n'
-import CreateDatasetForm from 'components/forms/CreateDatasetForm'
+import EditDatasetForm from 'components/forms/EditDatasetForm'
+import {useInvenioRecord} from '@oarepo/invenio-vue'
+import {useRoute} from 'vue-router'
 
 export default defineComponent({
-  name: 'CreateForm',
-  components: {CreateDatasetForm},
+  name: 'EditForm',
+  components: {EditDatasetForm},
   setup () {
     const {t} = useI18n()
+    const route = useRoute()
+    const record = ref(null)
+    const recordApi = route.path.substring(0, route.path.indexOf('/edit'))
+
+    record.value = useInvenioRecord(recordApi, {loadInitial: true})
 
     const header = computed(() => {
       return {
-        title: 'route.title.datasetUpload',
-        icon: 'addchart'
+        title: ` ${t('route.title.datasetEdit')} `,
+        icon: 'edit'
       }
     })
 
     useMeta(() => {
-      return {title: header.value? t(header.value.title) : ''}
+      return {title: header.value? `${t(header.value.title)} ${record.value.metadata? record.value.metadata.id : ''}` : ''}
     })
 
-    return {header}
+    return {header, record}
   }
 })
 </script>
