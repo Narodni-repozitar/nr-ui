153217bedf6888bc83dd7670f21a87518104cf25
Miroslav Bauer <mirekys@users.noreply.github.com>
2021-10-26 11:11:42 +0200

    solve issues with auth callback not passing

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -3,7 +3,6 @@ import axios from 'axios'
 import {Dialog, useQuasar} from "quasar";
 import {usePopupLogin} from "@oarepo/vue-popup-login";
 import {loginOptions} from "src/constants";
-import createAuthRefreshInterceptor from "axios-auth-refresh";
 import {i18n} from "boot/i18n";
 import AuthRequiredDialog from "components/dialogs/AuthRequiredDialog";
 const {login} = usePopupLogin(loginOptions)
@@ -15,48 +14,32 @@ export default boot(async ({app}) => {
   axios.defaults.withCredentials = true
 
   // Function that will be called to refresh authorization
-  const refreshAuthLogic = failedRequest => {
-    const showPopupLogin = async () => {
-      const loginOutcome = await login()
-      console.log(loginOutcome)
+  axios.interceptors.response.use(function (response) {
+    return response
+  }, function (error) {
+    if (error.response.status === 401) {
+      if (error.config.retry401) { return Promise.reject(error) }
+      return new Promise((resolve, reject) => {
+        Dialog.create({
+          component: AuthRequiredDialog,
+          persistent: true,
+        }).onOk((loginOutcome) => {
+          if (loginOutcome) {
+            error.config.retry401 = true
+            axios.request(error.config).then((r) => {
+              resolve(r)
+            }).catch(err => {
+              reject(err)
+            })
+          }
+          reject()
+        }).onCancel(() => {
+          reject()
+        })
+      })
     }
-
-    const dialog = Dialog.create({
-      component: AuthRequiredDialog,
-      persistent: true,
-    }).onOk(() => {
-      console.log('OK')
-      return Promise.resolve()
-    }).onCancel(() => {
-      console.log('Cancel')
-      return Promise.reject()
-    })
-  }
-
-  // Instantiate the interceptor
-  createAuthRefreshInterceptor(axios, refreshAuthLogic, {pauseInstanceWhileRefreshing: true})
-
-  // axios.interceptors.response.use(function (response) {
-  //   return response
-  // }, function (error) {
-  //   console.log(error.response.data)
-  //   if (error.response.status === 401) {
-  //     return new Promise((resolve) => {
-  //       BottomSheet.create({
-  //         persistent: true,
-  //         message: 'Authentication required. Click on the button below to log in.',
-  //         actions: [{
-  //           label: 'Log in',
-  //           icon: 'vpn_key',
-  //           id: 'log in'
-  //         }]
-  //       }).onOk(() => {
-  //         resolve(login())
-  //       })
-  //     })
-  //   }
-  //   return Promise.reject(error)
-  // })
+    return Promise.reject(error)
+  })
 
   axios.defaults.headers.common = {
     'Accept': 'application/json',
